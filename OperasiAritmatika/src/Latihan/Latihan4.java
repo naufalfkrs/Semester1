7a7d97b4e240b18ce44abe048d251819cacac7c9
Naufal Fakhri Risnawa <101534192+naufalfkrs@users.noreply.github.com>
2022-03-18 16:08:15 +0700

    Add files via upload

---
+++ OperasiAritmatika/src/Latihan/Latihan4.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author asus
+ */
+public class Latihan4 {
+    public static void main(String[]args ){
+        int a = 60;    
+        int b = 13;    
+        System.out.println("nilai a="+a);
+        System.out.println("nilai b="+b);
+        
+        a+=1;
+        b+=1;
+        System.out.println("nilai a kedua="+a);
+        System.out.println("nilai b kedua="+b);
+        
+        int hasilA=61;
+        int hasilB=14;
+        System.out.println("nilai hasilA="+hasilA);
+        System.out.println("nilai hasilB="+hasilB);
+        
+        hasilA++;
+        hasilB--;
+        System.out.println("nilai a ketiga="+a);
+        System.out.println("nilai b ketiga="+b);
+        System.out.println("nilai hasilA kedua="+hasilA);
+        System.out.println("nilai hasilB kedua="+hasilB);
+    }
+}
---
+++ TipeData/src/Latihan1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author asus
+ */
+public class Latihan1 {
+  public static void main(String[] args) { // main program
+        // TODO code application logic here
+        String nama,kelas;
+        Double p,l,luas;
+        nama= "Naufal Fakhri Risnawa";
+        kelas= "XRPL7";
+        p= 10.0;
+        l= 7.5;
+        luas= p*l;
+        System.out.println("Nama: "+nama);
+        System.out.println("Kelas: "+kelas);
+        System.out.println("Luas persegi: "+luas + " m"+ " bisa digunakan untuk sepak bola");
+    }  
+}
